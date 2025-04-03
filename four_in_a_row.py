98937140ee71f638652a7d671dc112c306a7ada1
Leon Hosch <leon.hosch@continental.com>
2025-04-03 15:14:38 +0200

    Satisfied further requirements of pylint

--- four_in_a_row.py
+++ four_in_a_row.py
@@ -1,41 +1,43 @@
 import random
 
 def intsafeinput(text):
+    """ function which is used to safely get an integer input"""
     while True:
         try:
             integer = int(input(text))
             return integer
-        except:
+        except ValueError:
             print("Integer input expected\n")
 
 class FourInARow:
+    """ The game class containing the methods needed to run the game """
 
-    # initializing the game class
     def __init__(self):
+        """ initializing the game class """
         self.matrix = [[], [], [], [], [], [], []]
         self.width = len(self.matrix)
         self.height = 6
-        self.printplayfield()
+        self.print_playfield()
         self.gameplay()
         return
 
-    # prints the matrix so the player can see what the playfield looks like
-    # with the current moves shown on the board
-    def printplayfield(self):
+    def print_playfield(self):
+        """ prints the matrix so the player can see what the playfield looks like
+        with the current moves shown on the board """
         print(" -----------------------")
         for x_coordinate in range(self.height-1, -1, -1):
             print("|", end="  ")
             for entry in self.matrix:
                 try:
                     print(entry[x_coordinate], end="  ")
-                except:
+                except IndexError:
                     print("-", end="  ")
             print("|")
         print(" -----------------------")
         return
 
-    # gameplay loop of our connect-four game
     def gameplay(self):
+        """ gameplay loop of our connect-four game """
         maxmoves = self.width * self.height
         for counter in range(maxmoves):
             if counter % 2:
@@ -53,50 +55,49 @@ class FourInARow:
                     try:
                         if len(self.matrix[column]) < self.height:
                             break
-                        else:
-                            print("Column is filled already!\n")
-                    except:
+                        print("Column is filled already!\n")
+                    except IndexError:
                         print(f"There are only {self.width} columns!\n")
 
             self.matrix[column].append(symbol)
-            self.printplayfield()
+            self.print_playfield()
             if self.win_test([column, len(self.matrix[column])-1], symbol):
                 print(f"The player with the symbol '{symbol}' won the game!")
                 return
 
-    # check if the game has been won by adding up matching symbols in a row
     def win_test(self, coordinates, symbol):
+        """ check if the game has been won by adding up matching symbols in a row """
         directions = [(-1, 0), (1, 0), (-1, -1), (1, 1), (1, -1), (-1, 1), (0, -1)]
         counter = 0
         previous = 0
         for direction in directions:
-            symbol_in_row = self.direction_check(direction, symbol, [coordinates[0], coordinates[1]])
-            if symbol_in_row + previous >= 3:
+            win_con = self.direction_check(direction, symbol, [coordinates[0], coordinates[1]])
+            if win_con + previous >= 3:
                 return True
             if counter % 2:
                 previous = 0
             else:
-                previous = symbol_in_row
+                previous = win_con
             counter += 1
         return False
 
-    # check the given direction for matching symbols
     def direction_check(self, direction, symbol, coordinates):
+        """ check the given direction for matching symbols """
         erg = 0
         while True:
             coordinates[0] += direction[0]
             coordinates[1] += direction[1]
-            y_height = len(self.matrix[coordinates[0]])
-            if self.width > coordinates[0] >= 0 and y_height > coordinates[1] >= 0:
-                if self.matrix[coordinates[0]][coordinates[1]] == symbol:
-                    erg += 1
+            if self.width > coordinates[0] >= 0:
+                if len(self.matrix[coordinates[0]]) > coordinates[1] >= 0:
+                    if self.matrix[coordinates[0]][coordinates[1]] == symbol:
+                        erg += 1
                 else:
                     return erg
             else:
                 return erg
 
-    # the AI of the bot player
     def bot_play(self):
+        """ the AI of the bot player """
         while True:
             move = random.randint(0, 6)
             if len(self.matrix[move]) < self.height:
